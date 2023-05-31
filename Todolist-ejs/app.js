e4d1bd1de788151cf710af9282a9a23c225a47b5
dev-ansh-r <rdevanshshukla26@gmail.com>
2023-05-31 07:21:33 +0530

    Todolist with express and ejs

---
+++ Todolist-ejs/app.js
@@ -0,0 +1,24 @@
+const app = require('express')();
+const bodyparser = require('body-parser');
+const ejs = require('ejs');
+
+app.set('view engine', 'ejs');
+app.use(bodyparser.urlencoded({extended: true}));
+
+let newItems = []
+app.get('/', (req, res) => {
+    let Options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    let today = new Date();
+    let day = today.toLocaleDateString('en-US',Options);
+    res.render("list", {KindOfDay: day, newListItems: newItems});
+});
+
+app.post('/', (req, res) => {
+    let newItem = req.body.newItem;
+    newItems.push(newItem);
+    res.redirect('/');
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
